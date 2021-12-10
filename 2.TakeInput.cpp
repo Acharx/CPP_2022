f49a79cbf583921b794d5ac849690c4f689843e4
Acharx <mehmet.achar@gmail.com>
2021-12-10 04:06:12 +0300

    Basic Concept

---
+++ 2.TakeInput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+
+    cin>>x;
+    cout<<"Input value is "<<x<<endl;
+
+    
+}
---
+++ 9.FunctionAndReturn.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b,int c){
+    cout<<"Summation"<<endl;
+    return a+b+c;
+}
+
+int main(){
+    int a = sum(1,2,3);
+    cout<<"a is "<<a<<endl;
+    
+
+}
